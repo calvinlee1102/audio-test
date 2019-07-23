8492755d08a1af64125a2d0e32992c85cdce1983
calvinlee1102 <45276042+calvinlee1102@users.noreply.github.com>
2019-07-23 22:33:09 +0800

    new

--- index.js
+++ index.js
@@ -50,3 +50,82 @@ function normalize(x) {
     const std = 10;
     return x.map(x => (x - mean) / std);
 }
+
+function getVoiceModel() {
+    const model = tf.sequential();
+    model.add(tf.layers.conv2d({
+        inputShape: [43, 232, 1],
+        kernelSize: [2, 8],
+        filters: 8,
+        strides: [1, 1],
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+    }));
+    model.add(tf.layers.maxPooling2d({
+        poolSize: [2, 2],
+        strides: [2, 2]
+    }));
+    model.add(tf.layers.conv2d({
+        kernelSize: [2, 4],
+        filters: 32,
+        strides: [1, 1],
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+    }));
+    model.add(tf.layers.maxPooling2d({
+        poolSize: [2, 2],
+        strides: [2, 2]
+    }));
+    model.add(tf.layers.conv2d({
+        kernelSize: [2, 4],
+        filters: 32,
+        strides: [1, 1],
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+    }));
+    model.add(tf.layers.maxPooling2d({
+        poolSize: [2, 2],
+        strides: [2, 2]
+    }));
+    model.add(tf.layers.conv2d({
+        kernelSize: [2, 4],
+        filters: 32,
+        strides: [1, 1],
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+    }));
+    model.add(tf.layers.maxPooling2d({
+        poolSize: [2, 2],
+        strides: [1, 2]
+    }));
+    model.add(tf.layers.flatten());
+    model.add(tf.layers.dropout({
+        rate: 0.25
+    }));
+    model.add(tf.layers.dense({
+        units: 2000,
+        kernelInitializer: 'varianceScaling',
+        activation: 'softmax'
+    }));
+    model.add(tf.layers.dropout({
+        rate: 0.5
+    }));
+    model.add(tf.layers.dense({
+        units: 20,
+        kernelInitializer: 'varianceScaling',
+        activation: 'softmax'
+    }));
+    //var optimizer = tf.train.sgd(LEARNING_RATE);
+    //alert(optimizertype);
+    //if (optimizertype =='sgd') {
+    //    optimizer = tf.train.sgd(LEARNING_RATE);
+    //} else if (optimizertype == 'momentum') {
+    //    optimizer = tf.train.momentum(LEARNING_RATE);
+    //}
+    //model.compile({
+    //    optimizer: optimizer,
+    //    loss: 'categoricalCrossentropy',
+    //    metrics: ['accuracy']
+    //});
+    return model;
+}
