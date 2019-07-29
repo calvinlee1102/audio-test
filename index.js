3cb1e6d4ed05f79fa25ae6879a26c5c9200da773
calvinlee1102 <45276042+calvinlee1102@users.noreply.github.com>
2019-07-29 14:24:45 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -76,17 +76,17 @@ async function train() {
 }
 
 function buildModel() {
-    //const model2 = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/speech-commands/v0.3/browser_fft/18w/model.json');
-    model = tf.sequential();
-    model.add(tf.layers.depthwiseConv2d({
-        depthMultiplier: 8,
-        kernelSize: [NUM_FRAMES, 3],
-        activation: 'relu',
-        inputShape: INPUT_SHAPE
-    }));
-    model.add(tf.layers.maxPooling2d({ poolSize: [1, 2], strides: [2, 2] }));
-    model.add(tf.layers.flatten());
-    //model = tf.sequential({layers: model2.layers.slice(0,11)});
+    const model2 = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/speech-commands/v0.3/browser_fft/18w/model.json');
+    //model = tf.sequential();
+    //model.add(tf.layers.depthwiseConv2d({
+    //    depthMultiplier: 8,
+    //    kernelSize: [NUM_FRAMES, 3],
+    //    activation: 'relu',
+    //    inputShape: INPUT_SHAPE
+    //}));
+    //model.add(tf.layers.maxPooling2d({ poolSize: [1, 2], strides: [2, 2] }));
+    //model.add(tf.layers.flatten());
+    model = tf.sequential({layers: model2.layers.slice(0,12)});
     model.add(tf.layers.dense({ units: classes, activation: 'softmax' }));
 
     const optimizer = tf.train.adam(0.01);
