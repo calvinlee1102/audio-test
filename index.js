8fcb33568207d5e0fad68816d9d0c682ea94f92c
calvinlee1102 <45276042+calvinlee1102@users.noreply.github.com>
2019-07-29 15:12:50 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ async function app() {
     //getVoiceModel();
 }
 
+// One frame is ~23ms of audio.
+const NUM_FRAMES = 43;
+let examples = [];
+const INPUT_SHAPE = [NUM_FRAMES, 232, 1];
+var classes = 3;
+let model;
+
 app();
 
 function collect(label) {
@@ -47,12 +54,6 @@ function normalize(x) {
     return x.map(x => (x - mean) / std);
 }
 
-// One frame is ~23ms of audio.
-const NUM_FRAMES = 43;
-let examples = [];
-const INPUT_SHAPE = [NUM_FRAMES, 232, 1];
-var classes = 3;
-let model;
 
 async function train() {
     toggleButtons(false);
@@ -75,17 +76,17 @@ async function train() {
 }
 
 async function buildModel() {
-    //const model2 = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/speech-commands/v0.3/browser_fft/18w/model.json');
-    model = tf.sequential();
-    model.add(tf.layers.depthwiseConv2d({
-        depthMultiplier: 8,
-        kernelSize: [NUM_FRAMES, 3],
-        activation: 'relu',
-        inputShape: INPUT_SHAPE
-    }));
-    model.add(tf.layers.maxPooling2d({ poolSize: [1, 2], strides: [2, 2] }));
-    model.add(tf.layers.flatten());
-    //model = tf.sequential({layers: model2.layers.slice(0,12)});
+    const model2 = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/speech-commands/v0.3/browser_fft/18w/model.json');
+    //model = tf.sequential();
+    //model.add(tf.layers.depthwiseConv2d({
+    //    depthMultiplier: 8,
+    //    kernelSize: [NUM_FRAMES, 3],
+    //    activation: 'relu',
+    //    inputShape: INPUT_SHAPE
+    //}));
+    //model.add(tf.layers.maxPooling2d({ poolSize: [1, 2], strides: [2, 2] }));
+    //model.add(tf.layers.flatten());
+    model = tf.sequential({layers: model2.layers.slice(0,12)});
     model.add(tf.layers.dense({ units: classes, activation: 'softmax' }));
 
     const optimizer = tf.train.adam(0.01);
@@ -207,8 +208,9 @@ function listen() {
         const input = tf.tensor(vals, [1, ...INPUT_SHAPE]);
         const probs = model.predict(input);
         const predLabel = probs.argMax(1);
-        const label = (await labelTensor.data())[0];
-        document.getElementById('console').textContent = labeltext[label];
+        var labeltemp = (await labelTensor.data())[0];
+        alert(labeltemp)
+        document.getElementById('console').textContent = labeltext[labeltemp];
         //await moveSlider(predLabel);
         tf.dispose([input, probs, predLabel]);
     }, {
